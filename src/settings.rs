b6dbe48c86594e35d5551ab8a224b63e1e0ed7ce
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2026-02-27 18:07:51 -0800

    Added more battles to battle splits

--- src/settings.rs
+++ src/settings.rs
@@ -5,121 +5,191 @@ use asr::settings::gui::Title;
 pub struct Settings {
     /// Auto Reset when returning to Main Menu
     #[default = false]
-    pub reset: bool,
+    reset: bool,
 
     /// Turn On NG+ Run
     #[default = false]
-    pub ng_plus: bool,
+    ng_plus: bool,
 
     /// Start of Act Splits
     _start_of_act_splits: Title,
     /// Act 1
-    pub act_1_start: bool,
+    act_1_start: bool,
     /// Act 2
-    pub act_2_start: bool,
+    act_2_start: bool,
     /// Act 3
-    pub act_3_start: bool,
+    act_3_start: bool,
 
     /// Prologue
     _prologue: Title,
     /// Maelle Fight
-    pub maelle_tutorial: bool,
+    maelle_tutorial: bool,
 
     /// Act 1
     _act_1: Title,
     /// Spring Meadows
     _spring_meadows: Title,
     /// First Lancelier
-    pub sm_first_lancelier: bool,
+    sm_first_lancelier: bool,
     /// First Portier
-    pub sm_first_portier: bool,
+    sm_first_portier: bool,
     /// First Volesters
-    pub sm_first_volesters: bool,
+    sm_first_volesters: bool,
     /// Évêque
-    pub sm_eveque: bool,
+    sm_eveque: bool,
     /// Flying Waters
-    pub _flying_waters: Title,
+    _flying_waters: Title,
     /// Curator
-    pub fw_curator: bool,
+    fw_curator: bool,
     /// Goblu
-    pub flw_goblu: bool,
+    fw_goblu: bool,
     /// Ancient Sanctuary
     _ancient_sanctuary: Title,
     /// Petank
-    pub as_petank: bool,
+    as_petank: bool,
     /// Robust Sakapatate
-    pub as_robust_sakapatate: bool,
+    as_robust_sakapatate: bool,
     /// Ultimate Sakapatate
-    pub as_ultimate_sakapatate: bool,
+    as_ultimate_sakapatate: bool,
     /// Gestral Village
     _gestral_village: Title,
     /// Bertrand Big Hands
-    pub gv_bertrand: bool,
+    gv_bertrand: bool,
     /// Dominique Giant Feet
-    pub gv_dominique: bool,
+    gv_dominique: bool,
     /// Matthieu The Colossus
-    pub gv_matthieu: bool,
+    gv_matthieu: bool,
     /// Sciel
-    pub gv_sciel: bool,
+    gv_sciel: bool,
     /// Esquie's Nest
     _esquies_nest: Title,
     /// François
-    pub en_francois: bool,
+    en_francois: bool,
+    /// The Continent
+    _wm_en_swc: Title,
+    /// Cruler / Lancelier / Demineur
+    wm_cruler_lancelier_demineur: bool,
     /// Stone Wave Cliffs
     _stone_wave_cliffs: Title,
     /// Lampmaster
-    pub swc_lampmaster: bool,
+    swc_lampmaster: bool,
 
     /// Act 2
     _act_2: Title,
     /// Forgotten Battlefield
     _forgotten_battlefield: Title,
     /// Chalier
-    pub fb_chalier: bool,
+    fb_chalier: bool,
     /// Dualliste
-    pub fb_dualliste: bool,
+    fb_dualliste: bool,
+    /// The Continent
+    _wm_fb_ms: Title,
+    /// Troubadour / Gault / Demineur
+    wm_troubadour_gault_demineur: bool,
     /// Monoco Station
     _monoco_station: Title,
     /// Monoco
-    pub ms_monoco: bool,
+    ms_monoco: bool,
     /// Stalact
-    pub ms_stalact: bool,
+    ms_stalact: bool,
     /// Old Lumiere
     _old_lumiere: Title,
     /// Ceramic & Steel Chevalière
-    pub ol_chevaliers: bool,
+    ol_chevaliers: bool,
     /// Renoir
-    pub ol_renoir: bool,
+    ol_renoir: bool,
     /// Visages
     _visages: Title,
     /// Mask Keeper
-    pub visages_mask_keeper: bool,
+    visages_mask_keeper: bool,
     /// Sirène
     _sirene: Title,
     /// Glissando
-    pub sirene_glissando: bool,
+    sirene_glissando: bool,
     /// Sirène
-    pub sirene_sirene: bool,
+    sirene_sirene: bool,
     /// The Monolith
     _monolith: Title,
     /// Fake Paintress
-    pub monolith_feetress: bool,
+    monolith_feetress: bool,
     /// Renoir
-    pub monolith_renoir: bool,
+    monolith_renoir: bool,
     /// The Paintress
-    pub monolith_paintress: bool,
+    monolith_paintress: bool,
 
     /// Act 3
     _act_3: Title,
     /// Return To Lumière
     _lumiere: Title,
     /// Renoir
-    pub lumiere_renoir: bool,
+    lumiere_renoir: bool,
     /// Verso
-    pub lumiere_verso: bool,
+    lumiere_verso: bool,
     /// Maelle
-    pub lumiere_maelle: bool,
+    lumiere_maelle: bool,
 
     /// Optional Encounter Splits
     _optional_encounters: Title,
+    /// Francois (Esquie Relationship LV6)
+    quest_francoisduel: bool,
+    /// Verso's Drafts
+    _versos_drafts: Title,
+    /// Balloon Gestral (Trigger Happy)
+    vd_balloon_gestral: bool,
+    /// Monsieur Frappe
+    vd_frappe: bool,
+    /// Osquio
+    vd_osquio: bool,
+
+    /// Yellow Harvest
+    _yellow_harvest: Title,
+    /// Glaise
+    yh_glaise: bool,
+    /// Merchant
+    yh_merchant: bool,
+
+    /// The Continent
+    _the_continent: Title,
+    /// Sprong
+    wm_sprong: bool,
+    /// Serpenphare
+    wm_serpenphare: bool,
+
+    /// Old Lumiere
+    _ol_optionals: Title,
+    /// Chromatic Danseuse
+    ol_chromatic_danseuse: bool,
+
+    /// Flying Manor
+    _flying_manor: Title,
+    /// Dualliste
+    fm_dualliste: bool,
+    /// Goblu
+    fm_goblu: bool,
+    /// Gargant
+    fm_gargant: bool,
+    /// Lampmaster
+    fm_lampmaster: bool,
+    /// Évêques
+    fm_eveques: bool,
+    /// Clea
+    fm_clea: bool,
+
+    /// Renoir's Drafts
+    _renoirs_drafts: Title,
+    /// Simon P1
+    rd_simon_p1: bool,
+    /// Simon P2
+    rd_simon_p2: bool,
+
+    /// Endless Tower
+    _endless_tower: Title,
+    /// Chromatic Lampmaster
+    et_chromatic_lampmaster: bool,
+    /// Clea Unleashed
+    et_clea_unleashed: bool,
+    /// Duolliste
+    et_duolliste: bool,
+    /// Simon The Divergent Star
+    et_simon_tds: bool,
 }
